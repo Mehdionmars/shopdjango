c70c45929f991271d06341b4d6d8264efc374cb4
mehdionmars <mehdimoungids@gmail.com>
2022-06-30 22:08:59 +0100

    init

---
+++ accounts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model, login, logout, authenticate
+from django.urls import reverse
+
+from store.models import Product, Order, Cart
+
+User = get_user_model()
+
+
+def signup(request):
+    if request.method == "POST":
+        # Traiter le formulaire
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = User.objects.create_user(username=username,
+                                        password=password)
+        login(request, user)
+        return redirect('index')
+
+    return render(request, 'accounts/signup.html')
+
+
+def login_user(request):
+    if request.method == "POST":
+        # connecter l'utilisateur
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('index')
+
+    return render(request, 'accounts/login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
+
